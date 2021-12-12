05332cedbca735f37ae543e7f2b083fb85a6a397
Deepak Jack <93731368+DeepakJack007@users.noreply.github.com>
2021-12-12 16:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import asyncio
 from pytgcalls import idle
-from driver.veez import call_py, bot
+from driver.Alice Zuberg import call_py, bot
 
 async def mulai_bot():
-    print("[VEEZ]: STARTING BOT CLIENT")
+    print("[Alice Zuberg]: STARTING BOT CLIENT")
     await bot.start()
-    print("[VEEZ]: STARTING PYTGCALLS CLIENT")
+    print("[Alice Zuberg]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
     await pidle()
-    print("[VEEZ]: STOPPING BOT & USERBOT")
+    print("[Alice Zuberg]: STOPPING BOT & USERBOT")
     await bot.stop()
 
 loop = asyncio.get_event_loop()
